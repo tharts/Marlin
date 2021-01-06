277b7328a381d7a58a3ab0ae1eb347f1856efaf3
Thomas Arts <thomas.arts@gmail.com>
2021-01-06 13:11:24 +0100

    add automatic versioning

---
+++ buildscript_versioning.py
@@ -0,0 +1,114 @@
+FILENAME_BUILDNO = 'versioning'
+FILENAME_VERSION_H = 'Marlin/Version.h'
+version = 'v2.0.7.2.'
+
+import datetime
+
+build_no = 0
+try:
+    with open(FILENAME_BUILDNO) as f:
+        build_no = int(f.readline()) + 1
+except:
+    print('Starting build number from 1..')
+    build_no = 1
+with open(FILENAME_BUILDNO, 'w+') as f:
+    f.write(str(build_no))
+    print('Build number: {}'.format(build_no))
+
+hf = f"""
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+////////////////////////////
+// VENDOR VERSION EXAMPLE //
+////////////////////////////
+
+/**
+ * Marlin release version identifier
+ */
+#define SHORT_BUILD_VERSION "{version+str(build_no)}"
+
+/**
+ * Verbose version identifier which should contain a reference to the location
+ * from where the binary was downloaded or the source code was compiled.
+ */
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+
+/**
+ * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
+ * here we define this default string as the date where the latest release
+ * version was tagged.
+ */
+#define STRING_DISTRIBUTION_DATE "{datetime.datetime.now().date()}"
+
+/**
+ * Defines a generic printer name to be output to the LCD after booting Marlin.
+ */
+#define MACHINE_NAME "3D-Makinator Dual"
+
+/**
+ * The SOURCE_CODE_URL is the location where users will find the Marlin Source
+ * Code which is installed on the device. In most cases —unless the manufacturer
+ * has a distinct Github fork— the Source Code URL should just be the main
+ * Marlin repository.
+ */
+#define SOURCE_CODE_URL "https://github.com/tharts/Marlin"
+
+/**
+ * Default generic printer UUID.
+ */
+#define DEFAULT_MACHINE_UUID "8eca0461-abac-4cc0-9b47-a9e87487292b"
+
+/**
+ * The WEBSITE_URL is the location where users can get more information such as
+ * documentation about a specific Marlin release.
+ */
+#define WEBSITE_URL "https://github.com/tharts/Marlin"
+
+/**
+ * Set the vendor info the serial USB interface, if changable
+ * Currently only supported by DUE platform
+ */
+//#define USB_DEVICE_VENDOR_ID           0x0000
+//#define USB_DEVICE_PRODUCT_ID          0x0000
+//#define USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
+
+
+
+
+
+"""
+with open(FILENAME_VERSION_H, 'w+') as f:
+    f.write(hf)
+
+'''
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "{}"
+#endif
+#ifndef VERSION
+  #define VERSION "{} - {}"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "{}"
+#endif
+'''
